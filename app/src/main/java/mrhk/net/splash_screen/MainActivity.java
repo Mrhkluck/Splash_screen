90f6f237dc2afb9b24ddb601424d478517e382ce
Rashid Humayun <mrhkluck@gmail.com>
2021-01-30 16:26:10 +0530

    Splash_screen

---
+++ app/src/main/java/mrhk/net/splash_screen/MainActivity.java
@@ -0,0 +1,27 @@
+package mrhk.net.splash_screen;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide(); // to hide action bar//
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Second_activity.class);
+                startActivity(intent);
+                finish();
+                //not again
+            }
+        },5000);
+    }
+}
---
+++ app/src/main/java/mrhk/net/splash_screen/Second_activity.java
@@ -0,0 +1,14 @@
+package mrhk.net.splash_screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Second_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_activity);
+    }
+}
